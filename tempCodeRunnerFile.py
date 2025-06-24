a76eb0760c200237afded559607165e6d49bf4ff
Tulsi Datt Saraswat <tulsidattsaraswat2005@gmail.com>
2025-06-24 15:28:42 +0530

    Update HTML UI and input validation logic

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-@app.route('/predict', methods=['POST'])
-def predict():
-    symptoms = [int(x) for x in request.form.values()]
-    df = pd.DataFrame([symptoms])
-    result = model.predict(df)[0]
-    return render_template("index.html", prediction_text=f"Predicted Disease: {result}")
